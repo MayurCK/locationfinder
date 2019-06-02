3a68e06b70011802e980e0f69b7a7f1985f46c63
MayurCK <hingnekar.mayur@gmail.com>
2019-06-02 19:41:41 +0530

    Delete IFourSquareService.java

--- src/main/java/com/travisca/app/locationfinder/service/IFourSquareService.java
+++
@@ -1,7 +0,0 @@
-package com.travisca.app.locationfinder.service;
-
-import com.travisca.app.locationfinder.dto.foursquare.Response;
-
-public interface IFourSquareService extends ISearchService{
-    Response callService(String city, String category );
-}
